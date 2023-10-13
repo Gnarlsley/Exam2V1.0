f110fab01a37ed0c95ccb75bb6ba0e1d6740f786
gnarlsley <thompson.garrett7413@gmail.com>
2023-10-13 13:38:37 -0500

    Added functionality to Add and Delete buttons. The corresponding controller action submits the data to the SQL table and commits. Additionally, I removed some extra code in the _Layout.cshtml file.

--- Exam2V1.0/Controllers/ProductController.cs
+++ Exam2V1.0/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Exam2V1._0.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Diagnostics;
+using Exam2V1._0.Migrations;
 
 namespace Exam2V1._0.Controllers
 {
@@ -13,15 +15,61 @@ namespace Exam2V1._0.Controllers
 
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Add()
         {
-            var products = context.Products.OrderBy(p => p.ProductId).ToList();
-            return View(products);
+            ViewBag.Action = "Add";
+            return View("Edit", new Product());
         }
-
-        public IActionResult Product()
+        [HttpGet]
+        public IActionResult Edit(int id)
         {
-            return View();
+            ViewBag.Action = "Confirm";
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ProductId == 0)
+                {
+                    context.Products.Add(product);
+                }
+                else
+                {
+                    context.Products.Update(product);
+                }
+                    context.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                if (product.ProductId == 0)
+                {
+                    ViewBag.Name = "Add";
+                }
+                else
+                {
+                    ViewBag.Name = "Edit";
+                }
+                return View(product);
+            }
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
